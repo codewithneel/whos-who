e4ed4634e4e1916a8adfbf0ecc54175866db11c2
Neel Patel <neel@Neels-MacBook-Pro.local>
2024-12-09 12:36:50 -0500

    Structure setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,18 @@ import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
+import { SettingsComponent } from './app/settings/settings.component';
+import { LeaderboardComponent } from './leaderboard/leaderboard.component';
+import { GameComponent } from './game/game.component';
+import { ResultsComponent } from './results/results.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ButtonComponent } from './components/button/button.component';
+import { ResultComponent } from './result/result.component';
 
 const routes: Routes = [{ path: "", component: HomeComponent }];
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, SettingsComponent, LeaderboardComponent, GameComponent, ResultsComponent, NavbarComponent, ButtonComponent, ResultComponent],
   imports: [BrowserModule, FormsModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
